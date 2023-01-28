8e6bc5396a8e48f42c2cee7e8b7e3998c77a6e8d
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-01-27 19:38:44 -0500

    will create the basic layout

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 const ship = (lengthVal, pointsVal, sunkStat) => {
     let Ship = {
         length: lengthVal,
-        // hit() {
-        //     points += 1;
-        // },
+        //hit() {points += 1;},
         points: pointsVal,
         sunk: sunkStat
     };
@@ -17,6 +15,26 @@ const isSunk = (Ship) => {
     }
     return Ship;
 };
+
+const gameBoard = () => {
+    for (let i = 0; i < 100; i++) {
+        const grid = document.getElementById('grid');
+        const secondGrid = document.getElementById('secondGrid');
+        const box = document.createElement('div');
+        const secondBox = document.createElement('div');
+        box.id = 'p1';
+        secondBox.id = 'p2';
+        grid.appendChild(box);
+        secondGrid.appendChild(secondBox);
+    }
+    box.addEventListener('click', () => {
+
+    })
+    secondBox.addEventListener('click', () => {
+
+    })
+}
+gameBoard();
 
 module.exports.ship = ship;
 module.exports.isSunk = isSunk;
