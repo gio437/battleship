3e5a86c7d6008cfa6eece0ba29255c4bfcf8e461
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-01-28 16:18:40 -0500

    button to create ship by typing coordinates adding to DOM

--- javascript.js
+++ javascript.js
@@ -1,41 +1,93 @@
-const ship = (lengthVal, pointsVal) => {
-    let Ship = {
-        length: lengthVal,
-        hit() {points += 1;},
-        points: pointsVal,
-        sunk: false
-    };
-    console.log(Ship);
-    return Ship;
-};
-
-const isSunk = (Ship) => {
-    if (Ship.length === Ship.points) {
-        Ship.sunk = true;
-        console.log(Ship);
-    }
-    return Ship;
-};
-
-const gameBoard = () => {
+const createGrid = () => {
     for (let i = 0; i < 100; i++) {
         const grid = document.getElementById('grid');
         const secondGrid = document.getElementById('secondGrid');
         const box = document.createElement('div');
         const secondBox = document.createElement('div');
-        box.id = 'p1';
-        secondBox.id = 'p2';
+        box.id = i;
+        secondBox.id = i;
+        box.classList.add('p1');
+        secondBox.classList.add('p1');
         grid.appendChild(box);
         secondGrid.appendChild(secondBox);
-        box.addEventListener('click', () => {
-
-        })
-        secondBox.addEventListener('click', () => {
-
-        })
     }
+}
+createGrid();
+
+const createShip = () => {
+    let carrier = {
+        length: 5,
+        hit() {points += 1;},
+        points: 0,
+        sunk: false
+    };
+    let battleship = {
+        length: 4,
+        hit() {points += 1;},
+        points: 0,
+        sunk: false
+    };
+    let destroyer = {
+        length: 3,
+        hit() {points += 1;},
+        points: 0,
+        sunk: false
+    };
+    let submarine = {
+        length: 3,
+        hit() {points += 1;},
+        points: 0,
+        sunk: false
+    };
+    let patroller = {
+        length: 2,
+        hit() {points += 1;},
+        points: 0,
+        sunk: false
+    };
+    let ships = {carrier, battleship, destroyer, submarine, patroller};
+    return ships;
+};
+
+const isSunk = (ship) => {
+    if (ship.length === ship.points) {
+        ship.sunk = true;
+        console.log(ship);
+    }
+    return ship;
+};
+
+const gameBoard = () => {
+   const placeShip = document.getElementById('ships');
+   placeShip.addEventListener('click', () => {
+        console.log(location);
+        let ships = createShip();
+        console.log(ships);
+        let carrierObj = ships.carrier;
+        console.log(carrierObj);
+        let carrierVal = window.prompt('place carrier using coordinates');
+        console.log(carrierVal);
+        return ships;
+     })
 }
 gameBoard();
 
-module.exports.ship = ship;
+const receiveAttack = () => {
+    if (attacked === 0) {
+        carrierObj.hit();
+    }
+    else {
+        //let missedShot = this.id;
+        console.log(missedShot);
+    }
+}
+
+const endGame = () => {
+    if (ships.carrier.sunk === true && ships.battleship.sunk === true && ships.destroyer.sunk === true && ships.submarine.sunk === true && ships.patroller.sunk === true) {
+        console.log('all ships sunk');
+    }
+}
+
+module.exports.createShip = createShip;
 module.exports.isSunk = isSunk;
+module.exports.gameBoard = gameBoard;
--- javascript.test.js
+++ javascript.test.js
@@ -1,7 +1,8 @@
-const ship = require('./javascript').ship;
+const createShip = require('./javascript').createShip;
 const isSunk = require('./javascript').isSunk;
+const gameBoard = require('./javascript').gameBoard;
 test('ship obj creation', () => {
-  expect(ship(2, 1, false)).toStrictEqual({length: 2, points: 1, sunk: false});
+  expect(createShip(2, 1, false)).toStrictEqual({length: 2, points: 1, sunk: false});
 });
 // let ships = ship(2, 2, false);
 // let ships2 = ship(2, 2 , true);
@@ -9,4 +10,8 @@ test('ship obj creation', () => {
 test('ship is sunk', () => {
     // console.log(ships);
   expect(isSunk({length: 2, points: 2, sunk: false})).toStrictEqual({length: 2, points: 2, sunk: true});
+});
+
+test('puts ships in coordinates', () => {;
+  expect(gameBoard()).toStrictEqual(ships);
 });
